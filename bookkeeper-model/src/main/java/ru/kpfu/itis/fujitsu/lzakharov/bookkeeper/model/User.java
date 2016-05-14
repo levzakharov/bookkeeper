278ccb534f83bc2cc0af6d0300278c2a93174f7b
Lev Zakharov <lev1996zaharov@gmail.com>
2016-05-14 18:05:19 +0300

    Add User model

---
+++ bookkeeper-model/src/main/java/ru/kpfu/itis/fujitsu/lzakharov/bookkeeper/model/User.java
@@ -0,0 +1,88 @@
+package ru.kpfu.itis.fujitsu.lzakharov.bookkeeper.model;
+
+import ru.kpfu.itis.fujitsu.lzakharov.bookkeeper.model.enums.Gender;
+
+/**
+ * Represents the User.
+ */
+public class User extends AbstractModel {
+    private Long id;
+    private String login;
+    private String password;
+    private Gender gender;
+
+    public User() {
+    }
+
+    public User(Long id, String login, String password, Gender gender) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.gender = gender;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (!id.equals(user.id)) return false;
+        if (!login.equals(user.login)) return false;
+        if (!password.equals(user.password)) return false;
+        return gender == user.gender;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + login.hashCode();
+        result = 31 * result + password.hashCode();
+        result = 31 * result + gender.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", gender=" + gender +
+                '}';
+    }
+}
