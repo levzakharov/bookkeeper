ae9ee1fa80d85bc24926ab66f296602faa21f3a1
Lev Zakharov <lev1996zaharov@gmail.com>
2016-06-02 14:35:40 +0300

    Add javadoc

--- bookkeeper-service/src/main/java/ru/kpfu/itis/fujitsu/lzakharov/bookkeeper/service/ClientAlreadyExistsException.java
+++ bookkeeper-service/src/main/java/ru/kpfu/itis/fujitsu/lzakharov/bookkeeper/service/ClientAlreadyExistsException.java
@@ -1,5 +1,8 @@
 package ru.kpfu.itis.fujitsu.lzakharov.bookkeeper.service;
 
+/**
+ * Signals that added client already exists.
+ */
 public class ClientAlreadyExistsException extends Exception {
     public ClientAlreadyExistsException(String message) {
         super(message);
